0a5ef5b53b8982c160060918a225c03a9c604de9
Aditya Mishra <adityamishra240405@gmail.com>
2025-06-23 10:03:31 +0530

    pusing

--- ARRAY/vectoroperation.cpp
+++ ARRAY/vectoroperation.cpp
@@ -3,28 +3,28 @@
 #include <algorithm>
 using namespace std ;
 int main() {
-vector <int> v;
-v.push_back(1);
-v.push_back(2);
-v.push_back(1);
-v.push_back(1);
-v.push_back(1);   //--------> puhback is used to insert
-v.push_back(1);  //             elements in the vector 
-v.push_back(4);
-v.push_back(4);
-v.push_back(0);
-v.push_back(1);
-// v.pop_back();
-// v.pop_back();
-// v.pop_back(); //-----------> pop back is used to delete the elements in the vector 
-// v.pop_back();
-// v.pop_back();
-// v.pop_back();
-v.size();  //-----------> used to get the size of the vector
-v.capacity();//---------->used to get the capacity of the vector 
-cout<<v.at(8)<<endl;
-sort(v.begin(),v.end());
-for(int i = 0 ;i<v.size();i++){
-    cout<<v[i]<<endl;
+vector <int> numb;
+numb.push_back(1);
+numb.push_back(2);
+numb.push_back(1);
+numb.push_back(1);
+numb.push_back(1);   //--------> puhback is used to insert
+numb.push_back(1);  //             elements in the vector 
+numb.push_back(4);
+numb.push_back(4);
+numb.push_back(0);
+numb.push_back(1);
+// numbpop_back();
+// numbpop_back();
+// numbpop_back(); //-----------> pop back is used to delete the elements in the vector 
+// numbpop_back();
+// numbpop_back();
+// numbpop_back();
+numb.size();  //-----------> used to get the size of the vector
+numb.capacity();//---------->used to get the capacity of the vector 
+cout<<numb.at(8)<<endl;
+sort(numb.begin(),numb.end());
+for(int i = 0 ;i<numb.size();i++){
+    cout<<numb[i]<<endl;
 }
 }
