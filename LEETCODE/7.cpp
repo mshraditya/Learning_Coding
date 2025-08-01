a01c1d191dfd9b764de215be7a1c495e288e1059
Aditya Mishra <adityamishra240405@gmail.com>
2025-08-01 21:50:51 +0530

    i am unstoppable

---
+++ LEETCODE/7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int reverse(int x){
+    
+
+}
+int main() {
+    int n;
+    cout<<"Enter The Number : ";
+    cin>>n;
+    reverse(n);
+    return 0;
+}
